143910352c14a973aaa6bd33bc0f93dbc0565e44
Jinyo-Chiu <q19860103g@gmail.com>
2016-09-28 13:25:19 +0800

    Update fb_initial.c

--- frameBuffer/fb_initial.c
+++ frameBuffer/fb_initial.c
@@ -1,4 +1,4 @@
-
+#include<stdio.h>
 #include "fb_initial.h"
 
 int FB_Create(fb_configure *fb_conf,char *DEVICE_NAME){
@@ -6,19 +6,19 @@ int FB_Create(fb_configure *fb_conf,char *DEVICE_NAME){
 	fb_conf->fb_handler = open(DEVICE_NAME,O_RDWR);
 		
 	if(!fb_conf->fb_handler){
-		printf("Error: cannot open framebuffer device.\n");
+		puts("Error: cannot open framebuffer device.\n");
 		return -1;
 	}
 
-	printf("The framebuffer device was opened successfully.\n");
+	puts("The framebuffer device was opened successfully.\n");
 
 	if(ioctl(fb_conf->fb_handler,FBIOGET_FSCREENINFO,&fb_conf->fix_info)){
-		printf("Error reading fixed information.\n");
+		puts("Error reading fixed information.\n");
 		return -1;
 	}
 
 	if(ioctl(fb_conf->fb_handler,FBIOGET_VSCREENINFO,&fb_conf->variable_info)){
-		printf("Error reading variable information.\n");
+		puts("Error reading variable information.\n");
 		return -1;
 	}
 
@@ -28,10 +28,10 @@ int FB_Create(fb_configure *fb_conf,char *DEVICE_NAME){
 	fb_conf->fbp = (char*)mmap(0,fb_conf->screensize,PROT_READ|PROT_WRITE,MAP_SHARED,fb_conf->fb_handler,0);
 
 	if((int)fb_conf->fbp==-1){
-		printf("Error: failed to map framebuffer device to memory.\n");
+		puts("Error: failed to map framebuffer device to memory.\n");
 	    return -1;	
 	}
-	printf("The framebuffer was mapped to memory successful.\n");
+	puts("The framebuffer was mapped to memory successful.\n");
 	
 	return 0;
 }
